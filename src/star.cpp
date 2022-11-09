ccb1b092d7eb67ca87c840e042847b38f51ba271
Kyle McCahill <kylemccahill@gmail.com>
2022-11-08 23:00:48 -0600

    converted example to project

---
+++ src/star.cpp
@@ -0,0 +1,18 @@
+#include "star.h"
+
+using namespace godot;
+
+void Star::_init() {
+    name = "test";
+    magnitude = float(4.83);
+}
+
+void Star::_process(float delta) {
+}
+
+
+void Star::_register_methods(){
+    register_method("_process", &Star::_process);
+    register_property<Star, String>("name", &Star::name, "test");
+    register_property<Star, float>("magnitude", &Star::magnitude, float(4.83));
+}
---
+++ src/star.h
@@ -0,0 +1,26 @@
+#ifndef STAR_H
+#define STAR_H
+
+#include <Godot.hpp>
+#include <Node2D.hpp>
+
+namespace godot {
+    class Star : public Node2D {
+        GODOT_CLASS(Star, Node2D)
+
+    private:
+        String name;
+        // float mass;             // think about good way to model this
+        // char spectral_type;
+        float magnitude;    // absoulute magnitude Mv is the measure of luminosity of an object on an inverse log astronomical magnitude scale. Basically how bright an object is when
+                            // viewed unobstructed from 10 parsecs away. range -20, 20?
+
+    public:
+        void _init();
+        void _process(float delta);
+
+        static void _register_methods();
+    };
+}
+
+#endif
